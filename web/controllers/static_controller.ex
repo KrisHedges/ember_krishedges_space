2b376200d9540a4414d002c73814276cab008eba
Kris Hedges <krishedges@gmail.com>
2016-01-22 18:14:08 -0500

    Read the static html files and send them directly avoiding the whole .html file extension

--- web/controllers/static_controller.ex
+++ web/controllers/static_controller.ex
@@ -1,16 +1,22 @@
 defmodule KrishedgesSpace.StaticController do
   use KrishedgesSpace.Web, :controller
 
-  def static_page(conn, params) do
+  def public(conn, params) do
     case conn.request_path do
       "/" ->
-        redirect conn, to: "/public/index.html"
-      "/admin" ->
-        redirect conn, to: "/admin/index.html"
-      "/admin/" ->
-        redirect conn, to: "/admin/index.html"
+        html conn, File.read! "priv/static/public/index.html"
+      _ ->
+        html conn, File.read! "priv/static/public/index.html"
     end
   end
 
+  def admin(conn, params) do
+    case conn.request_path do
+      "/" ->
+        html conn, File.read! "priv/static/admin/index.html"
+      _ ->
+        html conn, File.read! "priv/static/admin/index.html"
+    end
+  end
 
 end
