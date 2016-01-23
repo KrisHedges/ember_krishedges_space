e434db0f4a5ec6f785b230ccfce952272969b297
Kris Hedges <krishedges@gmail.com>
2016-01-23 18:32:03 -0500

    Better cache timing on static pages

--- web/controllers/static_controller.ex
+++ web/controllers/static_controller.ex
@@ -1,22 +1,19 @@
 defmodule KrishedgesSpace.StaticController do
   use KrishedgesSpace.Web, :controller
+  plug :add_cache_headers
 
   def public(conn, params) do
-    case conn.request_path do
-      "/" ->
-        html conn, File.read! "priv/static/public/index.html"
-      _ ->
-        html conn, File.read! "priv/static/public/index.html"
-    end
+    public = File.read! "priv/static/public/index.html"
+    html conn, public
   end
 
   def admin(conn, params) do
-    case conn.request_path do
-      "/" ->
-        html conn, File.read! "priv/static/admin/index.html"
-      _ ->
-        html conn, File.read! "priv/static/admin/index.html"
-    end
+    admin = File.read! "priv/static/admin/index.html"
+    html conn, admin
+  end
+
+  defp add_cache_headers(conn, _) do
+    Plug.Conn.put_resp_header(conn, "cache-control", "public, max-age=31536000")
   end
 
 end
