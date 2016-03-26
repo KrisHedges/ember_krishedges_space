1b4d316d5cb78ac7706590acff936bc6f0efeb22
Kris Hedges <krishedges@gmail.com>
2016-03-26 01:24:46 -0400

    Maybe no https?

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :krishedges_space, KrishedgesSpace.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "krishedges.space", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "krishedges.space", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
