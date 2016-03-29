cd6263457675c0f79b7a9d6f5e07765eee2cc878
Kris Hedges <krishedges@gmail.com>
2016-03-29 12:02:40 -0400

    trying magit from spacemacs

---
+++ ember_frontend/app/components/show-post.js
@@ -0,0 +1,21 @@
+/* global $:FALSE */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  didInsertElement: function(){
+    $('img[alt~="lightbox"]').wrap( "<div class='lightboxable'></div>" );
+    $('.lightboxable').unwrap('p')
+    $('.lightboxable').on('click', function(){
+      let image = $(this).find('img').attr('src');
+      $('.lightbox img').attr('src',image);
+      $('.lightbox').toggleClass('visible');
+    });
+    $('.lightbox').on('click', function(){
+      $(this).removeClass('visible');
+    });
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) $('.lightbox').click();
+    });
+  }
+});
+
