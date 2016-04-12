4d8e47bde1ff14b423c047d809dc948682b92ca9
Kris Hedges <krishedges@gmail.com>
2016-04-12 16:45:57 -0400

    some magit from spacemacs

--- web/controllers/s3_upload_controller.ex
+++ web/controllers/s3_upload_controller.ex
@@ -1,64 +1,64 @@
 defmodule KrishedgesSpace.S3UploadController do
-  use KrishedgesSpace.Web, :controller
-  use Guardian.Phoenix.Controller
+  # use KrishedgesSpace.Web, :controller
+  # use Guardian.Phoenix.Controller
 
-  plug :scrub_params, "file" when action in [:create]
-  plug :scrub_params, "path" when action in [:create]
+  # plug :scrub_params, "file" when action in [:create]
+  # plug :scrub_params, "path" when action in [:create]
 
-  plug Guardian.Plug.EnsureAuthenticated, %{ handler: { KrishedgesSpace.SessionController, :unauthenticated } }
+  # plug Guardian.Plug.EnsureAuthenticated, %{ handler: { KrishedgesSpace.SessionController, :unauthenticated } }
 
-  def index(conn, _params, _current_user, _claims) do
-    path = "priv/static/uploads/"
-    files = Path.wildcard("#{path}**/*")
-    #Build our object for the json
-    |> Enum.map(fn(filepath) ->
-      file_name = Regex.replace(~r/.*\//, filepath, "")
-      file_relative_path = Regex.replace(~r/priv\/static\/uploads\//, filepath, "")
-      %{ :filename => file_name, :path => "/#{file_relative_path}", :type => elem(File.stat(filepath),1).type }
-    end)
-    render(conn, KrishedgesSpace.UploadView, "index.json", uploads: files)
-  end
+  # def index(conn, _params, _current_user, _claims) do
+  #   path = "priv/static/uploads/"
+  #   files = Path.wildcard("#{path}**/*")
+  #   #Build our object for the json
+  #   |> Enum.map(fn(filepath) ->
+  #     file_name = Regex.replace(~r/.*\//, filepath, "")
+  #     file_relative_path = Regex.replace(~r/priv\/static\/uploads\//, filepath, "")
+  #     %{ :filename => file_name, :path => "/#{file_relative_path}", :type => elem(File.stat(filepath),1).type }
+  #   end)
+  #   render(conn, KrishedgesSpace.UploadView, "index.json", uploads: files)
+  # end
 
-  def create(conn, %{"file" => file_params, "path" => path_params}, _current_user, _claims) do
-    path = if path_params, do: path_params, else: ''
-    if Map.get(file_params, "type") === "directory" do
-      case File.mkdir_p("priv/static/uploads/#{path}/") do
-        :ok ->
-          conn
-          |> put_status(:created)
-          |> send_resp(:no_content, "")
-        {:error, reason} ->
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
-      end
-    else
-      case File.cp(file_params.path, "priv/static#{path}/#{URI.decode(file_params.filename)}") do
-        :ok ->
-          conn
-          |> put_status(:created)
-          |> send_resp(:no_content, "")
-        {:error, reason} ->
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
-      end
-    end
-  end
+  # def create(conn, %{"file" => file_params, "path" => path_params}, _current_user, _claims) do
+  #   path = if path_params, do: path_params, else: ''
+  #   if Map.get(file_params, "type") === "directory" do
+  #     case File.mkdir_p("priv/static/uploads/#{path}/") do
+  #       :ok ->
+  #         conn
+  #         |> put_status(:created)
+  #         |> send_resp(:no_content, "")
+  #       {:error, reason} ->
+  #         conn
+  #         |> put_status(:unprocessable_entity)
+  #         |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
+  #     end
+  #   else
+  #     case File.cp(file_params.path, "priv/static#{path}/#{URI.decode(file_params.filename)}") do
+  #       :ok ->
+  #         conn
+  #         |> put_status(:created)
+  #         |> send_resp(:no_content, "")
+  #       {:error, reason} ->
+  #         conn
+  #         |> put_status(:unprocessable_entity)
+  #         |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
+  #     end
+  #   end
+  # end
 
-  def delete(conn, params, _current_user, _claims) do
-    path = params["path"]
-    file_path = "priv/static/uploads#{path}"
-    case File.rm_rf file_path do
-      {:ok, _} ->
-        conn
-        |> put_status(:created)
-        |> send_resp(:no_content, "")
-      {:error, reason} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
-    end
-  end
+  # def delete(conn, params, _current_user, _claims) do
+  #   path = params["path"]
+  #   file_path = "priv/static/uploads#{path}"
+  #   case File.rm_rf file_path do
+  #     {:ok, _} ->
+  #       conn
+  #       |> put_status(:created)
+  #       |> send_resp(:no_content, "")
+  #     {:error, reason} ->
+  #       conn
+  #       |> put_status(:unprocessable_entity)
+  #       |> render(KrishedgesSpace.ErrorView, "error.json", message: reason)
+  #   end
+  # end
 end
 
