26531980a88ca7d948927a1232ab7ae13d7c94e5
Kris Hedges <krishedges@gmail.com>
2016-04-14 14:06:39 -0400

    Fixes for iOS header animation

--- ember_frontend/app/components/show-post.js
+++ ember_frontend/app/components/show-post.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function(){
     $('img[alt~="lightbox"]').wrap( "<div class='lightboxable'></div>" );
-    $('.lightboxable').unwrap('p')
+    $('.lightboxable').unwrap('p');
     $('.lightboxable').on('click', function(){
       let image = $(this).find('img').attr('src');
       $('.lightbox img').attr('src',image);
@@ -15,7 +15,9 @@ export default Ember.Component.extend({
       $(this).find('img').attr('src','');
     });
     $(document).keyup(function(e) {
-      if (e.keyCode == 27) $('.lightbox').click();
+        if (e.keyCode === 27) {
+          $('.lightbox').click();
+        }
     });
   }
 });
